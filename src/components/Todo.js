b8a953064f8b316da95880888c751c9c9b8f71f6
trongvuvan <>
2022-06-07 16:59:57 +0000

    add newtodo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,23 +1,18 @@
 import React, { useState } from 'react';
-
 /* 
   【Todoのデータ構成】
 　・key：Todoを特定するID（String）
 　・text：Todoの内容（String）
 　・done：完了状態（Boolean true:完了済み,, false:未完了）
 */
-
 /* コンポーネント */
 import TodoItem from './TodoItem';
 import Input from './Input';
 import Filter from './Filter';
-
 /* カスタムフック */
 import useStorage from '../hooks/storage';
-
 /* ライブラリ */
 import {getKey} from "../lib/util";
-
 function Todo() {
   const [items, putItems] = React.useState([
       /* テストコード 開始 */
@@ -26,9 +21,21 @@ function Todo() {
     { key: getKey(), text: '明日の準備をする', done: false },
     /* テストコード 終了 */
   ]);
+  
+  const handleCheck = checked => {
+    const newItems = items.map(item => {
+      if (item.key === checked.key) {
+        item.done = !item.done;
+      }
+      return item;
+    });
+    putItems(newItems);
+  };
+
   const handleAdd = text => {
     putItems([...items, { key: getKey(), text, done: false }]);
   };
+
   return (
     <div className="panel">
       <div className="panel-heading">
@@ -36,9 +43,10 @@ function Todo() {
       </div>
       <Input onAdd={handleAdd} />
       {items.map(item => (
-      <TodoItem
-        key={item.key}
-        item={item}
+        <TodoItem 
+          key={item.key}
+          item={item}
+          onCheck={handleCheck}
         />
       ))}
       <div className="panel-block">
@@ -47,5 +55,4 @@ function Todo() {
     </div>
   );
 }
-
 export default Todo;
