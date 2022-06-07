029ab59dd8b217d2c9dc877c45e33ad73345d5e3
trongvuvan <>
2022-06-07 17:16:16 +0000

    add tab

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -21,20 +21,25 @@ function Todo() {
     { key: getKey(), text: '明日の準備をする', done: false },
     /* テストコード 終了 */
   ]);
-  
+
+  const [filter, setFilter] = React.useState('ALL');
+
+  const displayItems = items.filter(item => {
+    if (filter === 'ALL') return true;
+    if (filter === 'TODO') return !item.done;
+    if (filter === 'DONE') return item.done;
+  });
+
+
   const handleCheck = checked => {
     const newItems = items.map(item => {
       if (item.key === checked.key) {
-        item.done = !item.done;
-      }
-      return item;
-    });
-    putItems(newItems);
-  };
-
+  function Todo() {
   const handleAdd = text => {
     putItems([...items, { key: getKey(), text, done: false }]);
   };
+
+  const handleFilterChange = value => setFilter(value);
 
   return (
     <div className="panel">
@@ -42,7 +47,11 @@ function Todo() {
         ITSS ToDoアプリ
       </div>
       <Input onAdd={handleAdd} />
-      {items.map(item => (
+      <Filter
+        onChange={handleFilterChange}
+        value={filter}
+      />
+      {displayItems.map(item => (
         <TodoItem 
           key={item.key}
           item={item}
@@ -50,9 +59,10 @@ function Todo() {
         />
       ))}
       <div className="panel-block">
-        {items.length} items
+        {displayItems.length} items
       </div>
     </div>
   );
 }
+        
 export default Todo;
